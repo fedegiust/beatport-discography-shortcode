1a2bf7dc89093c67b58048b7bd42157ba7ea927e
Federico Giust <fgiustdj@gmail.com>
2013-03-18 20:10:35 +0000

    Adding comments and tracks list

--- beatport-discography-shortcode.php
+++ beatport-discography-shortcode.php
@@ -26,72 +26,149 @@ class BeatportDiscography_shortcode {
 
 	var $plugin_version = '110'; // version 1.1.0
 
-	// Constructor / initialize plugin
+	/**
+	* Constructor / Initialize the plugin
+	*
+	* Pass the shortcode parameters to output_html to generate the output
+	*
+	* Get the beatport button for the admin side or the html for the public side
+	*/
 	function BeatportDiscography_shortcode()
 	{
 		// Load our shortcode only on the frontend
 		if ( !is_admin() )
 		{
-			
+			// Execute output_html to replace shortcode
 			add_shortcode('beatport_discography_sc', array($this, 'output_html'));
+			// Add css to the output
 			add_action('wp_enqueue_scripts', array($this, 'output_css'));
 		}
 		// Allow the button to appear on any rich text editor (i.e. text editor in a widget)
 		else
 		{
+			// If we are on the dashboard enable the beatport button
 			add_action('admin_init', array($this, 'setup_tinymce_button'));
 		}
 	}
 
-
+	/**
+	* Output CSS
+	* This gets the name of the CSS file we use to add some styling to the list of items
+	*/
 	function output_css()
 	{
 		wp_register_style('BeatportDiscographyShortcode', plugins_url('beatport-discography-shortcode.css', __FILE__));
 		wp_enqueue_style('BeatportDiscographyShortcode');
 	}
 
-
+	/**
+	* Generate unordered list with the items from the feed.
+	* Depending on the type of items we want, we generate the corresponding list.
+	* This function takes three parameters
+	* $items - Is the type of items to render (tracks or releases)
+	* $feed - Wich feed are we using, artist or label
+	* $dataArray - Array with the data we got from the API
+	*/
 	function getRenderedFeed($items, $feed, array $dataArray){
-		// echo '<pre>';
-		// print_r($data);
-		// echo '</pre>';
 
 		$output .= '<ul class="releaselist">' . PHP_EOL;
-		for ($i = 0; $i < count($dataArray['results']); $i++){
-			$artistsTemp = (array) $dataArray['results'][$i] -> artists; 
-			$output .= '<li class="releaserow">' . PHP_EOL;
-			$output .= '<div id="release' . $dataArray['results'][$i] -> catalogNumber . '" class="release">' . PHP_EOL;
-			$output .= '<div class="releaseart">' . PHP_EOL;
-			$output .= '<img src="' . $dataArray['results'][$i] -> images -> medium -> url . '"/>' . PHP_EOL;
-			$output .= '</div>' . PHP_EOL;
-			$output .= '<div class="releaseinfo">' . PHP_EOL;
-			$output .= '<span class="releasename">' . PHP_EOL;
-			$output .= $dataArray['results'][$i] -> name . PHP_EOL;
-			
-			$output .= '</span>' . PHP_EOL;
-			$output .= '<br />' . PHP_EOL;
-			$output .= '<span class="releaseartists">' . PHP_EOL;
-			for($j = 0; $j < count($artistsTemp); $j++){
-				$output .= $artistsTemp[$j] -> name;
-				if(count($artistsTemp)>0 && $j < count($artistsTemp)-1){
-					$output .= ', ';
+
+		if($items == 'releases'){
+			/** If we want the releases we then use the releases object from the API (http://api.beatport.com/releases.html) */
+
+			for ($i = 0; $i < count($dataArray['results']); $i++){
+				$artistsTemp = (array) $dataArray['results'][$i] -> artists; 
+				$output .= '<li class="releaserow">' . PHP_EOL;
+				$output .= '<div id="release' . $dataArray['results'][$i] -> catalogNumber . '" class="release">' . PHP_EOL;
+				$output .= '<div class="releaseart">' . PHP_EOL;
+				$output .= '<img src="' . $dataArray['results'][$i] -> images -> medium -> url . '"/>' . PHP_EOL;
+				$output .= '</div>' . PHP_EOL;
+				$output .= '<div class="releaseinfo">' . PHP_EOL;
+				$output .= '<span class="releasename">' . PHP_EOL;
+				$output .= $dataArray['results'][$i] -> name . PHP_EOL;
+				
+				$output .= '</span>' . PHP_EOL;
+				$output .= '<br />' . PHP_EOL;
+				$output .= '<span class="releaseartists">' . PHP_EOL;
+				for($j = 0; $j < count($artistsTemp); $j++){
+					$output .= $artistsTemp[$j] -> name;
+					if(count($artistsTemp)>0 && $j < count($artistsTemp)-1){
+						$output .= ', ';
+					}
 				}
+				
+				$output .= '</span>' . PHP_EOL;
+				$output .= '<br />' . PHP_EOL;
+				$output .= '<span class="releasemoreinfo">' . PHP_EOL;
+				$output .= $dataArray['results'][$i] -> catalogNumber . ' | ';
+				$output .= $dataArray['results'][$i] -> label -> name . ' | ';
+				$output .= $dataArray['results'][$i] -> releaseDate . ' | ' . PHP_EOL;
+				$output .= '<a href="https://www.beatport.com/release/' . $dataArray['results'][$i] -> slug . '/' . $dataArray['results'][$i] -> id . '" target="_new">Buy</a>';
+				$output .= $dataArray['results'][$i] -> genres -> name;
+				$output .= '</span>' . PHP_EOL;
+				$output .= '</div>' . PHP_EOL;
+				$output .= '</div>' . PHP_EOL;
+				$output .= '</li>' . PHP_EOL;
 			}
-			$output .= '</span>' . PHP_EOL;
-			$output .= '<br />' . PHP_EOL;
-			$output .= '<span class="releasemoreinfo">' . PHP_EOL;
-			$output .= $dataArray['results'][$i] -> catalogNumber . ' | ';
-			$output .= $dataArray['results'][$i] -> label -> name . ' | ';
-			$output .= $dataArray['results'][$i] -> releaseDate . ' | ' . PHP_EOL;
-			$output .= '<a href="https://www.beatport.com/release/' . $dataArray['results'][$i] -> slug . '/' . $dataArray['results'][$i] -> id . '">Buy</a>';
-			$output .= $dataArray['results'][$i] -> genres -> name;
-			$output .= '</span>' . PHP_EOL;
-			$output .= '</div>' . PHP_EOL;
-			$output .= '</div>' . PHP_EOL;
-			$output .= '</li>' . PHP_EOL;
+
+		}elseif ($items == 'tracks'){
+			/** If we want the tracks, we then use the tracks object (http://api.beatport.com/tracks.html) */
+			for ($i = 0; $i < count($dataArray['results']); $i++){
+				$artistsTemp = (array) $dataArray['results'][$i] -> artists; 
+				$genreTemp = (array) $dataArray['results'][$i] -> genres;
+				
+				$output .= '<li class="releaserow">' . PHP_EOL;
+				$output .= '<div id="release' . $dataArray['results'][$i] -> catalogNumber . '" class="release">' . PHP_EOL;
+				$output .= '<div class="releaseart">' . PHP_EOL;
+				$output .= '<img src="' . $dataArray['results'][$i] -> images -> medium -> url . '"/>' . PHP_EOL;
+				$output .= '</div>' . PHP_EOL;
+				$output .= '<div class="releaseinfo">' . PHP_EOL;
+				$output .= '<span class="releasename">' . PHP_EOL;
+				$output .= $dataArray['results'][$i] -> title . ' [' . $dataArray['results'][$i] -> length . ']' . PHP_EOL;
+				
+				$output .= '</span>' . PHP_EOL;
+				$output .= '<br />' . PHP_EOL;		
+				$output .= '<span class="releaseartists">' . PHP_EOL;
+				for($j = 0; $j < count($artistsTemp); $j++){
+					$output .= $artistsTemp[$j] -> name;
+					if(count($artistsTemp)>0 && $j < count($artistsTemp)-1){
+						$output .= ', ';
+					}
+				}
+				$output .= '</span>' . PHP_EOL;
+				$output .= '<br />' . PHP_EOL;
+				$output .= '<span class="releasemoreinfo">' . PHP_EOL;
+				for($j = 0; $j < count($genreTemp); $j++){
+					$output .= $genreTemp[$j] -> name;
+					if(count($genreTemp)>0 && $j < count($genreTemp)-1){
+						$output .= ', ';
+					}
+				}
+				$output .= ' | ';		
+				$output .= $dataArray['results'][$i] -> label -> name . ' | ';
+				$output .= $dataArray['results'][$i] -> releaseDate . ' | ' . PHP_EOL;
+				$output .= '<a href="https://www.beatport.com/track/' . $dataArray['results'][$i] -> slug . '/' . $dataArray['results'][$i] -> id . '" target="_new">Buy</a>' . PHP_EOL;
+				$output .= '</span>' . PHP_EOL;
+				$output .= '<br />' . PHP_EOL;
+
+				$output .= '</div>' . PHP_EOL;
+				$output .= '</div>' . PHP_EOL;
+				$output .= '</li>' . PHP_EOL;
+			}
+
+		}else{
+			$output .= 'An error has ocurred.';
 		}
+
 		$output .= '</ul>' . PHP_EOL;
 		return $output;
+	}
+
+	function getData($url, $qrystring){
+		$json = file_get_contents( $url . $qrystring);        
+		$data = json_decode($json);
+		$dataArray = (array) $data;
+		return $dataArray;
 	}
 
 	function output_html( $atts, $content = null )
@@ -114,10 +191,10 @@ class BeatportDiscography_shortcode {
 		// HTML OUTPUT
 		$output = '';
 
-		$url = 'http://api.beatport.com/catalog/3/';
+		$url = 'http://' . $this -> get_server_host();
 
 		if($atts['items'] == 'releases'){
-			$url .= 'release';
+			$url .= 'releases';
 		}else{
 			$url .= 'tracks';
 		}
@@ -128,11 +205,10 @@ class BeatportDiscography_shortcode {
 			
 		}else{
 			$url .= 'labels';
-			$qrystring = '?facets[]=label:' . $atts['labelid'] . '&publishDateStart=2000-02-06&sortBy=publishDate%20desc&perPage=100';
+			$qrystring = '?facets[]=labelId:' . str_replace(' ', '+', $atts['labelid']) . '&publishDateStart=2000-02-06&sortBy=publishDate%20desc&perPage=100';
 		}
-		$json = file_get_contents( $url . $qrystring);        
-		$data = json_decode($json);
-		$dataArray = (array) $data;
+
+		$dataArray = $this->getData($url, $qrystring);
 
 		$output .= $this->getRenderedFeed($atts['items'], $atts['feed'], $dataArray);
 		return $output;
@@ -142,11 +218,7 @@ class BeatportDiscography_shortcode {
 
 	function get_server_host()
 	{
-		// if (preg_match('/^http\:\/\/([a-zA-Z0-9\-\_\.]+\.)(?:youtube\.com|youtu\.be)/i', $url, $matches)) {
-		//     return $matches[1];
-		// }
-
-		return 'api.beatport.com/catalog/3/releases';
+		return 'api.beatport.com/catalog/3/';
 	}
 
 	
